2d93d64d76626dd4a3eaa8c62ddc7936d7d39c7f
Gabe <gb@test.com>
2022-09-01 13:10:04 -0500

    bunch of changes

---
+++ add_my_name.py
@@ -0,0 +1,9 @@
+
+def main():
+    outfile = open('philosophers.txt', 'a')
+
+    outfile.write("Johnny Bhojwani\n")
+
+    outfile.close()
+
+main()
---
+++ write_philosophers.py
@@ -0,0 +1,14 @@
+import csv
+
+def main():
+    outfile = open('philosophers.txt', 'w')
+
+    outfile.write('John Locke' + '\n')
+    outfile.write('David Hume' + '\n')
+    outfile.write('Edmund Burke' + '\n')
+
+
+    outfile.write()
+
+
+main()
